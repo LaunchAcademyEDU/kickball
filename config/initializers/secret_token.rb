47e2ab3608b805ad06330a1f41ed26eabc449175
Dan Pickett <dan.pickett@launchware.com>
2013-08-19 09:44:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kickball::Application.config.secret_key_base = '00b996b04b8a030ca9bb0b893bbd1c25c3b7e4c32c720a476c1e9d432f1f530fb673c72c192af5708b4da622b693cd93605ea4a0f8c24a0abaadc76b8e20f537'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kickball::Application.config.session_store :cookie_store, key: '_kickball_session'
